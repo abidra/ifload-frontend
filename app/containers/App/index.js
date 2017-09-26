7568e9cac5cf37c3a11264a02f44d14a3b52f07e
Abid Rakhmansyah <bidnth@gmail.com>
2017-09-27 06:40:51 +0700

    Update style use ant.design

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,8 +14,8 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
@@ -28,20 +28,36 @@ const AppWrapper = styled.div`
 
 export default function App() {
   return (
-    <AppWrapper>
+    <Layout>
       <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
+        titleTemplate="%s - IFload"
+        defaultTitle="IFload - Upload, Berbagi dan Download File secara Gratis"
       >
-        <meta name="description" content="A React.js Boilerplate application" />
+        <meta name="description" content="Temukan, Bagikan, Upload dan Download File secara Gratis dengan kecepatan Unggah dan Unduh Tanpa Batas dengan Kemudahan dan Kenyamanan tanpa iklan menganggu" />
+        <meta name="Keywords" content="file sharing, free web space, online storage, share files, photo image music mp3 video sharing, Google Drive Sharer, file hosting, internet file sharing"/>
+        <meta name="robots" content="noodp"/>
+        <link rel="publisher" href="https://plus.google.com/107594278007641832514/"/>
+        <meta property="og:locale" content="id_ID"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="IFload - Upload, Berbagi dan Download File secara Gratis"/>
+        <meta property="og:site_name" content="IFload"/>
+        <meta property="fb:app_id" content="299616453793169"/>
+        <meta property="og:image" content="https://i.imgbox.com/5adBdazd.jpg"/>
+        <meta property="og:image:width" content="1024"/><meta property="og:image:height" content="500"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:description" content="Temukan, Bagikan, Upload dan Download File secara Gratis dengan kecepatan Unggah dan Unduh Tanpa Batas dengan Kemudahan dan Kenyamanan tanpa iklan menganggu"/>
+        <meta name="twitter:title" content="IFload - Upload, Berbagi dan Download File secara Gratis"/>
+        <meta name="twitter:site" content="@IFloadID"/>
+        <meta name="twitter:image" content="https://i.imgbox.com/5adBdazd.jpg"/>
+        <meta name="generator" content="IFload v.1"/>
+        <link href="//www.ifload.com/includes/assets/img/favicon.png" rel="icon" type="image/png"/>
+        <link href="//www.ifload.com/includes/assets/img/favicon.ico" rel="shortcut icon"/>
       </Helmet>
-      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
-    </AppWrapper>
+    </Layout>
   );
 }
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,18 +11,13 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
-
+import Sider from 'components/Sider';
+import Header from 'components/Header';
+import Content from 'components/Content';
 import injectReducer from 'utils/injectReducer';
 import injectSaga from 'utils/injectSaga';
 import { makeSelectRepos, makeSelectLoading, makeSelectError } from 'containers/App/selectors';
-import H2 from 'components/H2';
-import ReposList from 'components/ReposList';
-import AtPrefix from './AtPrefix';
-import CenteredSection from './CenteredSection';
-import Form from './Form';
-import Input from './Input';
-import Section from './Section';
-import messages from './messages';
+import { Layout } from 'antd';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
 import { makeSelectUsername } from './selectors';
@@ -50,40 +45,15 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
     return (
       <article>
         <Helmet>
-          <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
+          <meta name="description" content="Temukan, Bagikan, Upload dan Download File secara Gratis dengan kecepatan Unggah dan Unduh Tanpa Batas dengan Kemudahan dan Kenyamanan tanpa iklan menganggu" />
         </Helmet>
-        <div>
-          <CenteredSection>
-            <H2>
-              <FormattedMessage {...messages.startProjectHeader} />
-            </H2>
-            <p>
-              <FormattedMessage {...messages.startProjectMessage} />
-            </p>
-          </CenteredSection>
-          <Section>
-            <H2>
-              <FormattedMessage {...messages.trymeHeader} />
-            </H2>
-            <Form onSubmit={this.props.onSubmitForm}>
-              <label htmlFor="username">
-                <FormattedMessage {...messages.trymeMessage} />
-                <AtPrefix>
-                  <FormattedMessage {...messages.trymeAtPrefix} />
-                </AtPrefix>
-                <Input
-                  id="username"
-                  type="text"
-                  placeholder="mxstbr"
-                  value={this.props.username}
-                  onChange={this.props.onChangeUsername}
-                />
-              </label>
-            </Form>
-            <ReposList {...reposListProps} />
-          </Section>
-        </div>
+        <Layout>
+        <Sider />
+        <Layout>
+        <Header />
+        <Content />
+        </Layout>
+        </Layout>
       </article>
     );
   }
